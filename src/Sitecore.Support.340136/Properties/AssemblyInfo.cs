8959d9f488cb87a3645c4140df21b6f05a8dc6d9
Artem Chunikhin <artem.chunikhin@sitecore.net>
2019-07-08 17:51:00 +0300

    Initial

---
+++ src/Sitecore.Support.340136/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.340136")]
+[assembly: AssemblyProduct("Sitecore.Support.340136")]
+[assembly: ComVisible(false)]
